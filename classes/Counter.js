fa84df7ca66b0ead5b7f9e9e0f9b3aba2e95ee48
Takewi <gawi2k21@gmail.com>
2023-11-04 23:21:50 -0300

    refactor: Update Counter class

--- classes/Counter.js
+++ classes/Counter.js
@@ -1,11 +1,14 @@
+import dayjs from 'dayjs';
 import { v4 as uuid } from 'uuid'
 export default class Counter {
-    #id = null;
+    #id = '';
+    #createdAt = '';
     #name = '';
     #weight = 0;
     #value = 0;
     constructor({ name, weight }) {
         this.#id = uuid(name.concat(weight))
+        this.#createdAt = dayjs().toISOString()
         this.#name = name
         this.#weight = weight
     }
@@ -24,6 +27,9 @@ export default class Counter {
     }
     get totalWeight() {
         return this.#weight * this.#value
+    }
+    get createdAt() {
+        return this.#createdAt
     }
     // Setters
     set name(newValue) {
