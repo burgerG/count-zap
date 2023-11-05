a51b17386a06beccdd29b1977f6b76d1bc76a3f5
Takewi <gawi2k21@gmail.com>
2023-11-04 23:11:57 -0300

    feat: Add Counter class

---
+++ classes/Counter.js
@@ -0,0 +1,39 @@
+import { v4 as uuid } from 'uuid'
+export default class Counter {
+    #id = null;
+    #name = '';
+    #weight = 0;
+    #value = 0;
+    constructor({ name, weight }) {
+        this.#id = uuid(name.concat(weight))
+        this.#name = name
+        this.#weight = weight
+    }
+    // Getters
+    get id() {
+        return this.#id
+    }
+    get name() {
+        return this.#name
+    }
+    get weight() {
+        return this.#weight
+    }
+    get value() {
+        return this.#value
+    }
+    get totalWeight() {
+        return this.#weight * this.#value
+    }
+    // Setters
+    set name(newValue) {
+        this.#name = newValue
+    }
+    // Methods
+    increment() {
+        this.#value++
+    }
+    decrement() {
+        this.#value--
+    }
+}
