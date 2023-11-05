6ffbc96b21240d71b11b66f29684156c37ad9290
Takewi <gawi2k21@gmail.com>
2023-11-05 01:22:53 -0300

    feat: Implement main counters store methods

---
+++ store/counters/mutations.js
@@ -0,0 +1,19 @@
+import Vue from "vue"
+export default {
+    // Payload must be an instance of the Group class
+    ADD_GROUP(state, payload) {
+        Vue.set(state.groups, payload.id, payload)
+    },
+    // Payload must be a Group id
+    REMOVE_GROUP(state, payload) {
+        Vue.delete(state.groups, payload)
+    },
+    // Payload is destructured, groupId must be a Group Id and counter a Counter class instance
+    ADD_COUNTER(state, { groupId, counter }) {
+        Vue.set(state.groups[groupId].counters, counter.id, counter)
+    },
+    // Payload is destructured, groupId must be a Group Id and counterId a Counter Id
+    REMOVE_COUNTER(state, { groupId, counterId }) {
+        Vue.delete(state.groups[groupId].counters, counterId)
+    }
+}
