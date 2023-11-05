312e20c749851adef992589ae3463d2346df441f
Takewi <gawi2k21@gmail.com>
2023-11-05 20:35:57 -0300

    refactor: Update Counter and Group classes

--- classes/Counter.js
+++ classes/Counter.js
@@ -1,35 +1,19 @@
 import dayjs from 'dayjs';
 import { v4 as uuid } from 'uuid'
 export default class Counter {
-    #id = '';
-    #createdAt = '';
-    #name = '';
-    #weight = 0;
+    id = '';
+    createdAt = '';
+    name = '';
+    weight = 0;
     value = 0;
-    constructor({ name, weight }) {
-        this.#id = uuid(name.concat(weight))
-        this.#createdAt = dayjs().toISOString()
-        this.#name = name
-        this.#weight = weight
+    constructor({ id = undefined, createdAt = undefined, name, weight, }) {
+        this.id = id ?? uuid(name.concat(weight));
+        this.createdAt = createdAt ?? dayjs().toISOString();
+        this.name = name;
+        this.weight = weight;
     }
     // Getters
-    get id() {
-        return this.#id
-    }
-    get name() {
-        return this.#name
-    }
-    get weight() {
-        return this.#weight
-    }
     get totalWeight() {
-        return this.#weight * this.value
-    }
-    get createdAt() {
-        return this.#createdAt
-    }
-    // Setters
-    set name(newValue) {
-        this.#name = newValue
+        return this.weight * this.value
     }
 }
--- classes/Group.js
+++ classes/Group.js
@@ -1,28 +1,17 @@
 import dayjs from 'dayjs';
 import { v4 as uuid } from 'uuid'
-
-export default class Counter {
-    #id = null;
-    #createdAt = null;
-    #name = '';
-    counters = {}
-    constructor({ name }) {
-        this.#id = uuid(name)
-        this.#createdAt = dayjs().toISOString()
-        this.#name = name
-    }
-    // Getters
-    get id() {
-        return this.#id
-    }
-    get name() {
-        return this.#name
-    }
-    get createdAt() {
-        return this.#createdAt
-    }
-    // Setters
-    set name(newValue) {
-        this.#name = newValue
+import Counter from './Counter';
+export default class Group {
+    id = '';
+    createdAt = '';
+    name = '';
+    counters = {};
+    constructor({ id = undefined, createdAt = undefined, counters = {}, name }) {
+        this.id = id ?? uuid(name);
+        this.createdAt = createdAt ?? dayjs().toISOString();
+        this.name = name;
+        Object.keys(counters).forEach(key => {
+            this.counters[key] = new Counter(counters[key])
+        });
     }
 }
